a82ce30fb053281dee1654f6d10aeb671abb0884
feather <Brian608@163.com>
2023-06-26 21:37:25 +0800

    集成mybatis 官方代码生成器

--- member/src/main/java/org/feather/train/member/mapper/MemberMapper.java
+++ member/src/main/java/org/feather/train/member/mapper/MemberMapper.java
@@ -1,15 +1,31 @@
 package org.feather.train.member.mapper;
 
-/**
- * @projectName: train
- * @package: org.feather.train.member.mapper
- * @className: MemberMapper
- * @author: feather
- * @description: TODO
- * @since: 2023-06-25 21:23
- * @version: 1.0
- */
-public interface MemberMapper {
+import org.apache.ibatis.annotations.Param;
+import org.feather.train.member.domain.Member;
+import org.feather.train.member.domain.MemberExample;
 
-    int count();
+import java.util.List;
+
+public interface MemberMapper {
+    long countByExample(MemberExample example);
+
+    int deleteByExample(MemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Member record);
+
+    int insertSelective(Member record);
+
+    List<Member> selectByExample(MemberExample example);
+
+    Member selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByExample(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByPrimaryKeySelective(Member record);
+
+    int updateByPrimaryKey(Member record);
 }
